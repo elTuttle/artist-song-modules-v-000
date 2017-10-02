0ed31aebc9b3cd84ca32bb0e897f5ec61da72909
John Tuttle <thejwt@gmail.com>
2017-10-02 12:51:57 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -10,10 +10,6 @@ class Artist
 
   @@artists = []
 
-  #def self.find_by_name(name)
-  #  @@artists.detect{|a| a.name == name}
-  #end
-
   def initialize
     @@artists << self
     @songs = []
@@ -22,15 +18,7 @@ class Artist
   def self.all
     @@artists
   end
-
-#  def self.reset_all
-#    self.all.clear
-#  end
-
-#  def self.count
-#    @@artists.count
-#  end
-
+  
   def add_song(song)
     @songs << song
     song.artist = self
@@ -40,8 +28,8 @@ class Artist
     songs.each { |song| add_song(song) }
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+#  def to_param
+#    name.downcase.gsub(' ', '-')
+#  end
 
 end
