9d2aa261a441c70b9b9bad69781a5ef05b868f70
John Tuttle <thejwt@gmail.com>
2017-10-02 13:08:46 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -14,10 +14,10 @@ class Artist
 
   @@artists = []
 
-  #def initialize
-  #  self.class.all << self
-  #  @songs = []
-  #end
+  def initialize
+    super
+    @songs = []
+  end
 
   def self.all
     @@artists
