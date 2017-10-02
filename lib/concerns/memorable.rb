ce5623bcc18d50910ef52b92dbd002bd6e45e214
John Tuttle <thejwt@gmail.com>
2017-10-02 13:07:17 +0000

    automatically backed up by learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,11 +1,17 @@
 module Memorable
+  module ClassMethods
+    def reset_all
+      self.all.clear
+    end
 
-  def reset_all
-    self.all.clear
-  end
+    def count
+      self.all.count
+    end
 
-  def count
-    self.all.count
-  end
+  module InstanceMethods
+    def initialize
+      self.class.all << self
+      @songs = []
+    end
 
 end
